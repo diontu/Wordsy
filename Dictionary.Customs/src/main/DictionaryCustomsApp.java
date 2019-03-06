95029edf4e92dce88389ac4d984fc81c78724837
Dion Tu <diontu1@gmail.com>
2019-03-06 00:16:57 -0500

    layout structure for the design

--- Dictionary.Customs/src/main/DictionaryCustomsApp.java
+++ Dictionary.Customs/src/main/DictionaryCustomsApp.java
@@ -1,7 +1,10 @@
 package main;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class DictionaryCustomsApp extends JFrame{
 
@@ -9,7 +12,34 @@ public class DictionaryCustomsApp extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static final int WINWIDTH = 500;
+	private static final int WINHEIGHT = 400;
 
+	private static void createAndShowGUI() {
+		
+        //Create and set up the window.
+        JFrame frame = new JFrame("Dictionary Customs");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // create a main panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setPreferredSize(new Dimension(WINWIDTH, WINHEIGHT));
+        
+        // added a label
+        JLabel label2 = new JLabel("Hello World");
+        
+        // added components to mainPanel
+        mainPanel.add(label2);
+ 
+        //Add the testPanel.
+        frame.getContentPane().add(mainPanel);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
@@ -17,19 +47,4 @@ public class DictionaryCustomsApp extends JFrame{
             }
         });
 	}
-
-	private static void createAndShowGUI() {
-		
-        //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
 }
